3d5790c31f4a3db0f379f7221ac61fd2a2e487b1
datkinson <daa9@aber.ac.uk>
2012-10-23 16:36:40 +0100

    added some global vaiables for various sizings of different robot components

--- code/drive.cpp
+++ code/drive.cpp
@@ -2,9 +2,24 @@
 
 //declare how many degrees per step the stepper motors rotate
 degreesRotated = 1.8;
+
 //declare diameter of the wheel in mm
 wheelDiameter = 120;
 
+//declare radius of the wheel in mm
+wheelRadius = wheelDiameter / 2;
+
+//distance taveled in a single rotation
+singleRotationDistance = 3.14159 * wheelDiameter;
+
+//Number of steps for a single rotation
+stepsInRotation = 360 / degreesRotated;
+
+//Width of the robot in mm from the center of each wheel
+robotWidth = 200;
+
+//width of the wheel in mm
+wheelWidth = 20;
 
 void setup()
 {
