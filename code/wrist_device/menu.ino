0b6a940a67f32d9977c5f71416d746ad4b974a42
Daniel Atkinson <daa9@aber.ac.uk>
2012-10-30 13:47:04 +0000

    Menu added to wrist controller

---
+++ code/wrist_device/menu.ino
@@ -0,0 +1,29 @@
+void menu(){
+  if (activeButton!=prevButton){
+ if(activeButton==2){
+   if(menuPosition<5){
+     menuPosition++;
+     screen=true;
+     displayMenu();
+   }
+  }
+  if(activeButton==1){
+   if(menuPosition>0){
+     menuPosition--;
+     screen=true;
+     displayMenu();
+   }
+  }
+  if(activeButton==3){
+   mode=menuPosition;
+  screen=true; 
+  }
+  }
+}
+void displayMenu(){
+  if(screen){
+    String temp = ">"+menuItems[menuPosition]; 
+    writeScreen(temp,menuItems[menuPosition+1]);
+    screen=false;
+  }
+}
