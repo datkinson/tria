b547330f9ab8e54158abe7875ae0dc4683fae26a
Daniel Atkinson <daa9@aber.ac.uk>
2012-11-28 14:02:43 +0000

    switch case added to serial stepper controller
    recieves single character bytes to determine next direction to move.
    recieve form python program/controller?

--- code/tests/serial_stepper_control/serial_stepper_control.ino
+++ code/tests/serial_stepper_control/serial_stepper_control.ino
@@ -1,17 +1,21 @@
 /* 
 Author: Daniel Atkinson
-Version: 0.1
+Version: 0.2
 Controlling stepper motors via serial input 
  */
 #include <Stepper.h>
 
 //full rotation diveded by degrees turned per step
 const int fullTurn = 360/1.8;
+//initialise serial variable
+int incomingData = 0;
 
 //Make a motor object using the stepper library on the correct pins
 Stepper motorL(fullTurn, 40,41,42,43);
 Stepper motorR(fullTurn, 50,51,52,53);
 
+//Magic numbers
+int arbritraryMovement = 10;
 
 void setup() {
   // initialize the serial port:
@@ -19,17 +23,30 @@ void setup() {
 }
 
 void loop() {
-
+  checkSerial();
+  
 }
 
 void checkSerial(){
-  if Serial.available(){
-     
+  if (Serial.available() > 0){
+     incomingData = Serial.read();
+     //Switch case to determine what to do with the serial data
+     switch (incomingData){
+       case 'f':
+         moveForward(arbritraryMovement);
+         break;
+       case 'l':
+         turnLeft(arbritraryMovement);
+         break;
+       case 'r':
+         turnRight(arbritraryMovement);
+         break;
+     }
   }
 }
 
 void moveForward(int distance){
-  for (int i = 0, i > distance, i++){
+  for (int i = 0; i > distance; i++){
      // step one step:
     motorL.step(-1);
     motorR.step(-1);
@@ -38,7 +55,7 @@ void moveForward(int distance){
 }
 
 void turnLeft(int distance){
-  for (int i = 0, i > distance, i++){
+  for (int i = 0; i > distance; i++){
     motorL.step(1);
     motorR.step(-1);
     delay(10);
@@ -46,7 +63,7 @@ void turnLeft(int distance){
 }
 
 void turnRight(int distance){
-  for (int i = 0, i > distance, i++){
+  for (int i = 0; i > distance; i++){
     motorL.step(-1);
     motorR.step(1);
     delay(10);
