afd42d595c619de293f819ee712390f664ae00bf
datkinson <daa9@aber.ac.uk>
2012-12-04 16:52:20 +0000

    Added release code section and added serial controlled motor code to it

---
+++ code/release/drive_only/drive_only.ino
@@ -0,0 +1,95 @@
+/*
+Author: Daniel Atkinson
+Version: 0.3
+Controlling stepper motors via serial input
+*/
+#include <Stepper.h>
+
+//full rotation diveded by degrees turned per step
+const int fullTurn = 360/1.8;
+//initialise serial variable
+int incomingData = 0;
+
+//Make a motor object using the stepper library on the correct pins
+Stepper motorL(fullTurn, 40,41,42,43);
+Stepper motorR(fullTurn, 50,51,52,53);
+
+//Magic numbers
+int arbritraryMovement = 30;
+
+void setup() {
+  // initialize the serial port:
+  Serial.begin(57600);
+}
+
+void loop() {
+  checkSerial();
+
+}
+
+void checkSerial(){
+  if (Serial.available() > 0){
+    incomingData = Serial.read();
+    //Switch case to determine what to do with the serial data
+    switch (incomingData){
+    case 'f':
+      Serial.println("Moving forward");
+      moveForward(arbritraryMovement);
+      break;
+    case 'l':
+      Serial.println("Turning left");
+      turnLeft(arbritraryMovement);
+      break;
+    case 'r':
+      Serial.println("Turning right");
+      turnRight(arbritraryMovement);
+      break;
+    case 't':
+      Serial.println("Testing");
+      test();
+      break;
+    }
+  }
+}
+
+void moveForward(int distance){
+  for (int i = 0; i < distance; i++){
+    // step one step:
+    motorL.step(-1);
+    motorR.step(-1);
+    //delay is required for some reason - !to be tested more!
+    delay(10);
+  }
+}
+
+void moveBackward(int distance){
+  for (int i = 0; i < distance; i++){
+    motorL.step(1);
+    motorR.step(1);
+    delay(10);
+  }
+}
+
+void turnLeft(int distance){
+  for (int i = 0; i < distance; i++){
+    motorL.step(1);
+    motorR.step(-1);
+    delay(10);
+  }
+}
+
+void turnRight(int distance){
+  for (int i = 0; i < distance; i++){
+    motorL.step(-1);
+    motorR.step(1);
+    delay(10);
+  }
+}
+
+void test(){
+  moveForward(500);
+  turnLeft(200);
+  moveBackward(600);
+  turnRight(200);
+}
+
