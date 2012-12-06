f0026ab7437e1f0df88e5d1ec91024b001f59939
Daniel Atkinson <daa9@aber.ac.uk>
2012-12-06 21:05:57 +0000

    Seperated out classes
    moved motor and sonar code to their own files

--- code/release/reactive_avoid/reactive_avoid.ino
+++ code/release/reactive_avoid/reactive_avoid.ino
@@ -4,32 +4,22 @@ Version: 1.1
 Controlling stepper motors based on sonar sensor readings in a reactive way
 */
 #include <Stepper.h>
-
 #define sonar1TrigPin 8
 #define sonar1EchoPin 7
 #define sonar2TrigPin 6
 #define sonar2EchoPin 5
 
-//full rotation diveded by degrees turned per step
-const int fullTurn = 360/1.8;
 //initialise serial variable
 int incomingData = 0;
-
-//Make a motor object using the stepper library on the correct pins
-Stepper motorL(fullTurn, 40,41,42,43);
-Stepper motorR(fullTurn, 50,51,52,53);
 
 //Global variables
 int leftSonar, rightSonar;
 
 //Magic Numbers
-int sonarLower = 0;
-int sonarUpper = 400;
 int arbritraryMovement = 100;
-int proximityThreshold = 20;
 int motorDelay = 1000;
+int proximityThreshold = 20;
 int defaultDelay = 10;
-
 
 void setup() {
   // initialize the serial port:
@@ -39,76 +29,12 @@ void setup() {
   pinMode(sonar1EchoPin, INPUT);
   pinMode(sonar2TrigPin, OUTPUT);
   pinMode(sonar2EchoPin, INPUT);
-
 }
 
 void loop() {
   detectProximity();
   if(validateSonar){
     reactiveSonar(leftSonar, rightSonar);
-  }
-}
-
-
-void moveForward(int distance){
-  Serial.println("Moving Forward");
-  for (int i = 0; i < distance; i++){
-    // step one step:
-    motorL.step(-1);
-    motorR.step(-1);
-    //delay is required for some reason - !to be tested more!
-    delay(defaultDelay);
-  }
-}
-
-void moveBackward(int distance){
-  for (int i = 0; i < distance; i++){
-    motorL.step(1);
-    motorR.step(1);
-    delay(defaultDelay);
-  }
-}
-
-void turnLeft(int distance){
-  Serial.println("Turning Left");
-  for (int i = 0; i < distance; i++){
-    motorL.step(1);
-    motorR.step(-1);
-    delay(defaultDelay);
-  }
-}
-
-void turnRight(int distance){
-  Serial.println("Turning Right");
-  for (int i = 0; i < distance; i++){
-    motorL.step(-1);
-    motorR.step(1);
-    delay(defaultDelay);
-  }
-}
-
-int getSonarReading(int trigger, int echo){
-  int duration, distance;
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(motorDelay);
-  digitalWrite(trigger, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2) / 29.1;
-  return(distance);
-}
-
-void detectProximity(){
-  leftSonar = getSonarReading(sonar1TrigPin, sonar1EchoPin);
-  rightSonar = getSonarReading(sonar2TrigPin, sonar2EchoPin);
-  Serial.print("Left: ");
-  Serial.print(leftSonar);
-  Serial.print(" - Right: ");
-  Serial.println(rightSonar);
-  if (leftSonar < proximityThreshold){
-    turnRight(arbritraryMovement);
-  }
-  if (rightSonar < proximityThreshold){
-    turnLeft(arbritraryMovement);
   }
 }
 
@@ -124,16 +50,6 @@ void reactiveSonar(int left, int right){
       }else{
         moveForward(arbritraryMovement);
       }
-    }
-  }
-}
-
-bool validateSonar(){
-  if(leftSonar < sonarUpper && leftSonar > sonarLower){
-    if(rightSonar < sonarUpper && rightSonar > sonarLower){
-      return true;
-    }else{
-      return false;
     }
   }
 }
