b896e0e9f4d293a27dcf1fadd63f2a543606a26b
datkinson <daa9@aber.ac.uk>
2013-01-29 10:42:46 +0000

    Template added for Infrared sensors

---
+++ code/release/reactive_avoid/ir.ino
@@ -0,0 +1,31 @@
+//Magic numbers
+int irLower = 0;
+int irUpper = 400;
+
+int getIrReading(){
+  return(distance);
+}
+
+void detectProximity(){
+  leftIr = getIrReading(leftIrId);
+  rightIr = getIrReading(rightIrId);
+  Serial.print("Left: ");
+  Serial.print(leftIr);
+  Serial.print(" - Right: ");
+  Serial.println(rightIr);
+  if (leftIr < proximityThreshold){
+    turnIr(arbritraryMovement);
+  }
+  if (rightIr < proximityThreshold){
+    turnLeft(arbritraryMovement);
+  }
+}
+bool validateIr(){
+  if(leftIr < irUpper && leftIr > irLower){
+    if(rightIr < irUpper && rightIr > irLower){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
