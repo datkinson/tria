a197b8aab597a8ee9f6bed203c8aeda6a243af70
Daniel Atkinson <daa9@aber.ac.uk>
2013-03-29 12:21:35 +0000

    Detailed comments

--- code/tests/pololu_test/pololu_test.ino
+++ code/tests/pololu_test/pololu_test.ino
@@ -1,20 +1,28 @@
 void setup() {
-  pinMode(10, OUTPUT); //Step pin
-  pinMode(11, OUTPUT); //Direction pin
+  pinMode(13, OUTPUT); //Left direction pin
+  pinMode(12, OUTPUT); //Left step pin
+  pinMode(11, OUTPUT); //Right direction pin
+  pinMode(10, OUTPUT); //Right step pin
 }
 void loop() {
-  digitalWrite(11, HIGH); // +5V to Pin D1
+  digitalWrite(13, HIGH); //Set left motor forward
+  digitalWrite(11, LOW); //Set right motor forward
   for (int i = 0; i < 200; i++){
-    digitalWrite(10, HIGH); // +5V to Pin D2
+    digitalWrite(12, HIGH); //Step left wheel forward
+    digitalWrite(10, HIGH); //Step right wheel forward
     delay(10); // wait a 1/10 second
-    digitalWrite(10, LOW); // Gnd to Pin D2
+    digitalWrite(12, LOW); //Stop left wheel
+    digitalWrite(10, LOW); //Stop right wheel
     delay(10); // wait a 1/10 second
   }
-  digitalWrite(11, LOW);
+  digitalWrite(13, LOW); //reverse left motor deirection
+  digitalWrite(11, HIGH); //reverse right motor deirection
   for (int j = 0; j < 200; j++){
-    digitalWrite(10, HIGH); // +5V to Pin D2
+    digitalWrite(12, HIGH); //Step left wheel backwards
+    digitalWrite(10, HIGH); //Step right wheel backwards
     delay(10); // wait a 1/10 second
-    digitalWrite(10, LOW); // Gnd to Pin D2
+    digitalWrite(12, LOW); //Stop left wheel
+    digitalWrite(10, LOW); //Stop right wheel
     delay(10); // wait a 1/10 second
   }
 }
